c29d12ada0af9a89cec366dde2c85866dbd5416d
Natalya <nemia.me@gmail.com>
2022-10-18 22:42:05 +0300

    Домашнее задание 1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// ДЗ 1
+
+/*
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Input the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine($"max number: {num1}, min number: {num2}");
+else if (num2 > num1)
+    Console.WriteLine($"max number: {num2}, min number: {num1}");
+else
+    Console.WriteLine("The first number is equal to the second number.");
+*/
+
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Input the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2)
+{
+    if (num1 >= num3)
+        Console.WriteLine("max number: " + num1);
+    else
+        Console.WriteLine("max number: " + num3);
+}
+else
+{
+    if (num2 >= num3)
+        Console.WriteLine("max number: " + num2);
+    else
+        Console.WriteLine("max number: " + num3);
+}
+*/
+
+/*
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
+*/
+
+/*
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+
+while (current <= num)
+{
+    Console.Write($"{current} ");
+    current += 2;
+}
+*/
